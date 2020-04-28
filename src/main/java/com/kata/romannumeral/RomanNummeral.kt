0965cbce9607ff7485a508bcdcf1883e55b1da79
Shakeel Hussain <shakeel.hussain@careem.com>
2020-04-29 02:25:44 +0500

    GREEN: Statement -> tail recursion

--- src/main/java/com/kata/romannumeral/RomanNummeral.kt
+++ src/main/java/com/kata/romannumeral/RomanNummeral.kt
@@ -2,11 +2,17 @@ package com.kata.romannumeral
 
 class RomanNummeral {
     fun convert(arabicNumber: Int): String? {
-        val romanNumbers = mapOf(1 to "I", 4 to "IV", 5 to "V", 6 to "VI", 7 to "VII", 8 to "VIII")
+        val romanNumbers = mapOf(
+                1 to "I",
+                4 to "IV",
+                5 to "V")
         if (romanNumbers.containsKey(arabicNumber)) {
             return romanNumbers[arabicNumber]
         }
-        return "I" + convert(arabicNumber - 1);
+        if(arabicNumber<4)
+            return "I" + convert(arabicNumber - 1);
+
+        return convert(arabicNumber - 1)+"I";
     }
 
 }
