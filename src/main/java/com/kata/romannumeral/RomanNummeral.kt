ead71e99a1c0615471233f8a7fd9147348acc944
Shakeel Hussain <shakeel.hussain@careem.com>
2020-04-28 18:47:02 +0500

    GREEN: Array -> container,Statement -> tail recursion

--- src/main/java/com/kata/romannumeral/RomanNummeral.kt
+++ src/main/java/com/kata/romannumeral/RomanNummeral.kt
@@ -2,7 +2,7 @@ package com.kata.romannumeral
 
 class RomanNummeral {
     fun convert(arabicNumber: Int): String? {
-        val romanNumbers = mapOf(1 to "I", 4 to "IV", 5 to "V")
+        val romanNumbers = mapOf(1 to "I", 4 to "IV", 5 to "V", 6 to "VI", 7 to "VII", 8 to "VIII")
         if (romanNumbers.containsKey(arabicNumber)) {
             return romanNumbers[arabicNumber]
         }
--- src/test/java/com/kata/romannumeral/RomanNumeralsShould.kt
+++ src/test/java/com/kata/romannumeral/RomanNumeralsShould.kt
@@ -23,8 +23,6 @@ class RomanNumeralsShould {
         Assert.assertEquals("III", romanNummeral.convert(arabicNumber = 3))
     }
 
-
-
     @Test
     fun `given 4 should return IV in roman`() {
         val romanNummeral: RomanNummeral = RomanNummeral();
@@ -32,8 +30,26 @@ class RomanNumeralsShould {
     }
 
     @Test
-    fun `given 5 should return IV in roman`() {
+    fun `given 5 should return V in roman`() {
         val romanNummeral: RomanNummeral = RomanNummeral();
         Assert.assertEquals("V", romanNummeral.convert(arabicNumber = 5))
+    }
+
+    @Test
+    fun `given 6 should return VI in roman`() {
+        val romanNummeral: RomanNummeral = RomanNummeral();
+        Assert.assertEquals("VI", romanNummeral.convert(arabicNumber = 6))
+    }
+
+    @Test
+    fun `given 7 should return VII in roman`() {
+        val romanNummeral: RomanNummeral = RomanNummeral();
+        Assert.assertEquals("VI", romanNummeral.convert(arabicNumber = 6))
+    }
+
+    @Test
+    fun `given 8 should return VIII in roman`() {
+        val romanNummeral: RomanNummeral = RomanNummeral();
+        Assert.assertEquals("VIII", romanNummeral.convert(arabicNumber = 8))
     }
 }
